4e7e436816d5870e79e2031bc2efa6ddc84e78d5
Leandro Lemos Lima <leandro.freebsd@gmail.com.br>
2024-04-21 17:56:36 -0300

    Novas aulas e acertos

--- aula002.py
+++ aula002.py
@@ -1,3 +1,9 @@
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, 1011, sep="", end='#')
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
+
 # alguns caracteres de scape \r (retorno de carro \n (nova linha) -> CRLF windows versão antiga e as novas
 # \n -> LF unix
 # obs: print já faz quebra de linha automaticamente sem precisar dos scapes acima
@@ -5,3 +11,4 @@ print(12, 34, 105, sep ='-', end = '\r\n')
 print(56, 78, sep ="========", end = '\n')
 print(1, 2, 3, sep ="," , end = '#\n') 
 print(1, 2, 3, sep =",") 
+
